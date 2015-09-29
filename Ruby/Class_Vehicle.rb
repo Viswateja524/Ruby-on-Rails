4f318b60a3690963ac850cda3f82d20cfbb11a32
Viswateja Reddy <viswateja524@gmail.com>
2015-09-29 12:00:40 +0530

    Ruby

---
+++ Ruby/Class_Vehicle.rb
@@ -0,0 +1,9 @@
+class Customer
+	
+	def hello
+		puts "hello Ruby"
+	end
+end
+
+object = Customer. new
+object.hello
---
+++ Ruby/Comments_Ruby.rb
@@ -0,0 +1,12 @@
+# I am a comment. Just ignore me.
+name = "Viswateja" # This is again comment
+# This is a comment.
+# This is a comment, too.
+# This is a comment, too.
+# I said that already.
+=begin
+This is a comment.
+This is a comment, too.
+This is a comment, too.
+I said that already.
+=end
